0267c5252989a3257e5d904adffe058b23ffc15c
Matheus Vancoll <matheu-s_andre@hotmail.com>
2021-04-20 01:31:07 -0300

    try vercel error server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import PlacarOnline from './pages/Placar'
-import ConfigPlacarOnline from './pages/ConfigPlacar'
 
 function App() {
   return (
     <div className="App">
-      {/* <ConfigPlacarOnline /> */}
       <PlacarOnline timeA="Jurubeba Sport Club" timeB="Matutino Futebol Mananger" />
     </div>
   );
