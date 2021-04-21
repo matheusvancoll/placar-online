44f9a5de86bb1c5f25712fc005e8f16ed95d9d45
Matheus Vancoll <matheu-s_andre@hotmail.com>
2021-04-21 13:28:26 -0300

    clear pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import PlacarOnline from './pages/Placar'
-import ConfigPlacarOnline from './pages/ConfigPlacar'
 
 function App() {
   return (
     <div className="App">
-      <ConfigPlacarOnline />
       <PlacarOnline timeA="AMIGOS DO TIME" timeB="ACADÊMICOS DO SALGUEIRO" />
     </div>
   );
